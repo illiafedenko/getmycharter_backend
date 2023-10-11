ef6dd40030546486cd75111390ed5b3119f8e9ec
Illia Fedenko <illiafedenko69@gmail.com>
2023-10-11 19:58:23 +0200

    add google auth

---
+++ app/routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+//login by email
+router.post('/login', authController.login);
+
+//Sign up by email
+router.post('/signup', authController.signup);
+
+// Google Login
+router.post('/google', authController.google);
+
+module.exports = router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const authRouter = require('./auth');
 const userRouter = require('./users');
 const yachtRouter = require('./yacht');
 const policyRouter = require('./policy');
 const termsRouter = require('./terms');
 const FAQRouter = require('./FAQ');
 
+router.use('/auth', authRouter);
 router.use('/users', userRouter);
 router.use('/yachts', yachtRouter);
 router.use('/policy', policyRouter);
